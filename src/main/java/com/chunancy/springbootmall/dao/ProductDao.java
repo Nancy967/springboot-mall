25797ca0b713efe0ec5366368e5419cea35f1070
Nancy967 <beg7520@gmail.com>
2022-07-22 08:16:30 +0800

    商品功能:實作查詢商品列表(基礎)

--- src/main/java/com/chunancy/springbootmall/dao/ProductDao.java
+++ src/main/java/com/chunancy/springbootmall/dao/ProductDao.java
@@ -3,7 +3,10 @@ package com.chunancy.springbootmall.dao;
 import com.chunancy.springbootmall.dto.ProductRequest;
 import com.chunancy.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createproduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
--- src/main/java/com/chunancy/springbootmall/service/ProductService.java
+++ src/main/java/com/chunancy/springbootmall/service/ProductService.java
@@ -3,7 +3,10 @@ package com.chunancy.springbootmall.service;
 import com.chunancy.springbootmall.dto.ProductRequest;
 import com.chunancy.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+    List<Product> getProducts();
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
